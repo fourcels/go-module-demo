ea0cda3dbac0eb27b68d8475d5f4834423e10dd9
Kiyan <fourcels@gmail.com>
2023-03-29 09:21:54 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fourcels/go-module-demo
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,8 @@
+package gomoduledemo
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("hello world, this is my first publish module")
+	fmt.Println("this Hello func in top package")
+}
